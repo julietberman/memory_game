3e2f6ad68dc6bfd1a46c15d1c4d56f8972ca9711
julietberman <julietberman210@gmail.com>
2016-09-28 11:59:17 -0400

    added javascript functions to create cards

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-console.log("JS file is connected to HTML! Woo!")
+
+/* console.log("JS file is connected to HTML! Woo!")
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
 
 if ((cardOne === cardTwo) || (cardThree === cardFour)) {
 alert('You found a match!');
@@ -10,3 +12,17 @@ alert('You found a match!');
 else {
 alert('Sorry, try again.');
 }
+*/
+
+var gameBoard = document.getElementById('game-board');
+
+function createcards(){
+for (var i = 0; i < 4; i++)
+	{
+    var newCard = document.createElement('div');
+	newCard.className = 'card';
+	gameBoard.appendChild(newCard);
+	}
+}
+
+createcards();
